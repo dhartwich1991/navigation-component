5b5442a96c944f1ce16fc89e5598653c6ed2ebcb
daniel.hartwich <daniel.hartwich@xing.com>
2018-09-04 18:36:50 +0200

    Add Unit test

--- app/src/test/java/com/jdapplications/stationary/navigation/ExampleUnitTest.kt
+++ app/src/test/java/com/jdapplications/stationary/navigation/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.jdapplications.stationary.navigation
 
+import androidx.navigation.testing.TestNavigator
+import androidx.navigation.testing.TestNavigatorDestinationBuilder
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,15 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun navigatorTest() {
+        val testNavigator = TestNavigator()
+        val testNavigatorDestinationBuilder = TestNavigatorDestinationBuilder(testNavigator, R.id.overviewFragment)
+        val destination = testNavigatorDestinationBuilder.build()
+        testNavigator.navigate(destination, null, null)
+        assert(testNavigator.mBackStack.isNotEmpty())
+        assertEquals(testNavigator.mBackStack.first.first?.id, R.id.overviewFragment)
     }
 }
